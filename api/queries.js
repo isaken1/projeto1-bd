7aa5a65a6271520a8b5dfc05d535ae3e804476f1
Isaac Kennedy Diniz Gomes <isaac_kennedy@hotmail.com>
2019-10-21 19:35:27 -0300

    Novas queries criadas para o endpoint de dependentes, nova organização dos pacotes NPM, novos scripts NPM para instalação e execução dos sistemas.

--- api/queries.js
+++ api/queries.js
@@ -25,7 +25,7 @@ const createEmpregado = (request, response) => {
     const { nome, nomedomeio, sobrenome, codigo, dtnascimento, endereco,
         sexo, salario, gerente, departamento } = request.body;
 
-    pool.query('INSERT INTO empregado  (nome, nomedomeio, sobrenome, ' +
+    pool.query('INSERT INTO Empregado (nome, nomedomeio, sobrenome, ' +
         'codigo, dtNascimento, endereco, sexo, salario, gerente, ' +
         'departamento) VALUES $1, $2, $3, $4, $5, $6, $7, $8, $9, $10',
         [nome, nomedomeio, sobrenome, codigo, dtnascimento, endereco,
@@ -60,11 +60,14 @@ const deleteEmpregado = (request, response) => {
 const updateEmpregado = (request, response) => {
     const id = parseInt(request.params.id)
 
-    pool.query('UPDATE Empregados SET nome = $1, nomedomeio = $2, ' +
+    const { nome, nomeDoMeio, sobrenome, dtNascimento,
+         endereco, sexo, salario, gerente, departamento } = request.body
+
+    pool.query('UPDATE Empregado SET nome = $1, nomedomeio = $2, ' +
         'sobrenome = $3, dtNascimento = $4, endereco = $5, sexo = $6, ' +
         'salario = $7, gerente = $8, departamento = $9 WHERE codigo = $10',
-        [nome, nomedomeio, sobrenome, dtnascimento, endereco,
-            sexo, salario, gerente, departamento, codigo],
+        [nome, nomeDoMeio, sobrenome, dtNascimento, endereco,
+            sexo, salario, gerente, departamento, id],
         (error, result) => {
             if (error) {
                 response.status(500).json(error);
@@ -85,6 +88,60 @@ const getDependentes = (request, response) => {
             }
 
             response.status(200).json(results.rows);
+        }
+    );
+}
+
+const createDependente = (request, response) => {
+    const { empregado, nome, sexo, dtNascimento, parentesco } = request.body
+    pool.query('INSERT INTO Dependente  (nome, dtNascimento, sexo, parentesco, empregado)' +
+        ' VALUES $1, $2, $3, $4, $5', 
+        [nome, dtNascimento, sexo, parentesco, empregado],
+        (error, result) => {
+            if (error) {
+                response.status(500).send(error)
+                console.log(error)
+            }
+
+            response.status(201).send(`Dependente criado com o ID:
+                ${result.insertId}`)
+        }
+    );
+}
+
+const deleteDependente = (request, response) => {
+    const idEmpregado = parseInt(request.params.id) 
+    const nomeDependente = request.params.nomeDependente
+
+    pool.query('DELETE FROM Dependente WHERE idEmpregado = $1 AND nomeDependente = $2', 
+        [idEmpregado, nomeDependente],
+        (error, result) => {
+            if (error) {
+                response.status(500).send(error);
+                console.log(error);
+            }
+
+            response.status(200).send(`Dependente ${nomeDependente} deletado!`)
+        }
+    );
+}
+
+const updateDependente = (request, response) => {
+    const idEmpregado = parseInt(request.params.idEmpregado)
+    const nomeDependente = request.params.nomeDependente
+
+    const { sexo, dtNascimento, parentesco } = request.body
+
+    pool.query('UPDATE Dependente SET sexo = $1, parentesco = $2, dtNascimento = $3 ' +
+        'WHERE nome = $4 AND empregado = $5',        
+        [sexo, parentesco, dtNascimento, nomeDependente, idEmpregado],
+        (error, result) => {
+            if (error) {
+                response.status(500).json(error);
+                console.log(error);
+            }
+              
+        response.status(200).send(`Dependente ${nomeDependente} alterado!`)
         }
     );
 }
@@ -134,6 +191,9 @@ module.exports = {
     getEmpregados,
     updateEmpregado,
     getDependentes,
+    createDependente,
+    deleteDependente,
+    updateDependente,
     getDepartamentos,
     getLocais,
     getProjetos,
