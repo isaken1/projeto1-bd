e9cf2748c1d2c7039d2b7b1dc9239c6b841fce59
Isaac Kennedy Diniz Gomes <isaac_kennedy@hotmail.com>
2019-10-21 23:58:08 -0300

    Finalização dos endpoint básicos dos CRUDs das entidades, adição de um Linter (padrão AirBnB)

--- api/index.js
+++ api/index.js
@@ -1,40 +1,52 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+
+import db from './queries';
+
 const app = express();
 
 const port = 3000;
 
-app.use(bodyParser.json())
+app.use(json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  urlencoded({
+    extended: true,
+  }),
 );
 
-const db = require('./queries')
+// Endpoints CRUD de Empregados
+app.get('/empregados', db.getEmpregados);
+app.post('/empregados', db.createEmpregado);
+app.delete('/empregados/:id', db.deleteEmpregado);
+app.put('/empregados/:id', db.updateEmpregado);
 
-//Endpoints CRUD de Empregados
-app.get('/empregados', db.getEmpregados)
-app.post('/empregados', db.createEmpregado)
-app.delete('/empregados/:id', db.deleteEmpregado)
-app.put('/empregados/:id', db.updateEmpregado)
+// Endpoints CRUD de Dependentes
+app.get('/dependentes', db.getDependentes);
+app.post('/dependentes', db.createDependente);
+app.delete('/dependentes/:id/:nome', db.deleteDependente);
+app.put('dependentes/:id/:nome', db.updateDependente);
 
-//Endpoints CRUD de Dependentes
-app.get('/dependentes', db.getDependentes)
+// Endpoints CRUD de Departamentos
+app.get('/departamentos', db.getDepartamentos);
+app.post('/departamentos', db.createDepartamento);
+app.delete('/departamento/:id', db.deleteDepartamento);
+app.put('/departamento/:id', db.updateDepartamento);
 
-//Endpoints CRUD de Departamentos
-app.get('/departamentos', db.getDepartamentos)
+// Endpoints CRUD de Locais
+app.get('/locais', db.getLocais);
+app.post('/locais', db.createLocal);
+app.delete('/locais/:departamento/:nome', db.deleteLocal);
 
-//Endpoints CRUD de Locais
-app.get('/locais', db.getLocais)
-
-//Endpoints CRUD de Projetos
-app.get('/projetos', db.getProjetos)
+// Endpoints CRUD de Projetos
+app.get('/projetos', db.getProjetos);
+app.post('/projetos', db.createProjeto);
+app.put('/projetos/:id', db.updateProjeto);
+app.delete('/projetos/:id', db.deleteProjeto);
 
 app.get('/', (request, response) => {
-    response.json({ info: 'A api está funcionando!' })
+  response.json({ info: 'A API está funcionando!' });
 });
 
 app.listen(port, () => {
-    console.log(`A API está rodando na porta ${port}`)
+  console.log(`A API está rodando na porta ${port}`);
 });
--- api/queries.js
+++ api/queries.js
@@ -1,200 +1,345 @@
-const Pool = require('pg').Pool;
+import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: 'isaac',
-    host: 'localhost',
-    database: 'empresa_api',
-    password: 'abcd1234',
-    port: 5432
+  user: 'isaac',
+  host: 'localhost',
+  database: 'empresa_api',
+  password: 'abcd1234',
+  port: 5432,
 });
 
 const getEmpregados = (request, response) => {
-    pool.query('SELECT * FROM Empregado ORDER BY nome',
-        (error, results) => {
-            if (error) {
-                response.status(500).send(error);
-                console.log(error);
-            }
+  pool.query('SELECT * FROM Empregado ORDER BY nome',
+    (error, results) => {
+      if (error) {
+        response.status(500).send(error);
+        console.log(error);
+      }
 
-            response.status(200).json(results.rows);
-        }
-    );
-}
+      response.status(200).json(results.rows);
+    });
+};
 
 const createEmpregado = (request, response) => {
-    const { nome, nomedomeio, sobrenome, codigo, dtnascimento, endereco,
-        sexo, salario, gerente, departamento } = request.body;
+  const {
+    nome, nomedomeio, sobrenome, codigo, dtnascimento, endereco,
+    sexo, salario, gerente, departamento,
+  } = request.body;
 
-    pool.query('INSERT INTO Empregado (nome, nomedomeio, sobrenome, ' +
-        'codigo, dtNascimento, endereco, sexo, salario, gerente, ' +
-        'departamento) VALUES $1, $2, $3, $4, $5, $6, $7, $8, $9, $10',
-        [nome, nomedomeio, sobrenome, codigo, dtnascimento, endereco,
-            sexo, salario, gerente, departamento],
-        (error, result) => {
-            if (error) {
-                response.status(500).send(error);
-                console.log(error);
-            }
+  pool.query('INSERT INTO Empregado (nome, nomedomeio, sobrenome, '
+        + 'codigo, dtNascimento, endereco, sexo, salario, gerente, '
+        + 'departamento) VALUES $1, $2, $3, $4, $5, $6, $7, $8, $9, $10',
+  [nome, nomedomeio, sobrenome, codigo, dtnascimento, endereco,
+    sexo, salario, gerente, departamento],
+  (error, result) => {
+    if (error) {
+      response.status(500).send(error);
+      console.log(error);
+    }
 
-            response.status(201).send(`Empregado criado com o ID:
+    console.log(result);
+    response.status(201).send(`Empregado criado com o ID:
                 ${result.insertId}`);
-        }
-    );
-}
+  });
+};
 
 const deleteEmpregado = (request, response) => {
-    const id = parseInt(request.params.id) 
+  const idEmpregado = parseInt(request.params.id, 10);
 
-    pool.query('DELETE FROM Empregado WHERE codigo = $1', [1],
-        (error, result) => {
-            if (error) {
-                response.status(500).send(error)
-                console.log(error)
-            }
+  pool.query('DELETE FROM Empregado WHERE codigo = $1', [idEmpregado],
+    (error, result) => {
+      if (error) {
+        response.status(500).send(error);
+        console.log(error);
+      }
 
-            response.status(200).send(`Empregado código ${id} deletado!`)
-        }
-    );
-}
+      console.log(result);
+      response.status(200).send(`Empregado código ${idEmpregado} deletado!`);
+    });
+};
 
 const updateEmpregado = (request, response) => {
-    const id = parseInt(request.params.id)
+  const id = parseInt(request.params.id, 10);
 
-    const { nome, nomeDoMeio, sobrenome, dtNascimento,
-         endereco, sexo, salario, gerente, departamento } = request.body
+  const {
+    nome, nomeDoMeio, sobrenome, dtNascimento,
+    endereco, sexo, salario, gerente, departamento,
+  } = request.body;
 
-    pool.query('UPDATE Empregado SET nome = $1, nomedomeio = $2, ' +
-        'sobrenome = $3, dtNascimento = $4, endereco = $5, sexo = $6, ' +
-        'salario = $7, gerente = $8, departamento = $9 WHERE codigo = $10',
-        [nome, nomeDoMeio, sobrenome, dtNascimento, endereco,
-            sexo, salario, gerente, departamento, id],
-        (error, result) => {
-            if (error) {
-                response.status(500).json(error);
-                console.log(error);
-            }
-              
-        response.status(200).send(`Empregado código ${id} alterado!`)
-        }
-    );
-}
+  pool.query('UPDATE Empregado SET nome = $1, nomedomeio = $2, '
+        + 'sobrenome = $3, dtNascimento = $4, endereco = $5, sexo = $6, '
+        + 'salario = $7, gerente = $8, departamento = $9 WHERE codigo = $10',
+  [nome, nomeDoMeio, sobrenome, dtNascimento, endereco,
+    sexo, salario, gerente, departamento, id],
+  (error, result) => {
+    if (error) {
+      response.status(500).json(error);
+      console.log(error);
+    }
+
+    console.log(result);
+    response.status(200).send(`Empregado código ${id} alterado!`);
+  });
+};
 
 const getDependentes = (request, response) => {
-    pool.query('SELECT * FROM Dependente ORDER BY nome',
-        (error, results) => {
-            if (error) {
-                response.status(500).json(error);
-                console.log(error);
-            }
+  pool.query('SELECT * FROM Dependente ORDER BY nome',
+    (error, results) => {
+      if (error) {
+        response.status(500).json(error);
+        console.log(error);
+      }
 
-            response.status(200).json(results.rows);
-        }
-    );
-}
+      response.status(200).json(results.rows);
+    });
+};
 
 const createDependente = (request, response) => {
-    const { empregado, nome, sexo, dtNascimento, parentesco } = request.body
-    pool.query('INSERT INTO Dependente  (nome, dtNascimento, sexo, parentesco, empregado)' +
-        ' VALUES $1, $2, $3, $4, $5', 
-        [nome, dtNascimento, sexo, parentesco, empregado],
-        (error, result) => {
-            if (error) {
-                response.status(500).send(error)
-                console.log(error)
-            }
+  const {
+    empregado, nome, sexo, dtNascimento, parentesco,
+  } = request.body;
 
-            response.status(201).send(`Dependente criado com o ID:
-                ${result.insertId}`)
-        }
-    );
-}
+  pool.query('INSERT INTO Dependente  (nome, dtNascimento, sexo, parentesco, empregado)'
+        + ' VALUES $1, $2, $3, $4, $5',
+  [nome, dtNascimento, sexo, parentesco, empregado],
+  (error, result) => {
+    if (error) {
+      response.status(500).send(error);
+      console.log(error);
+    }
+
+    console.log(result);
+    response.status(201).send(`Dependente criado com o ID:
+                ${result.insertId}`);
+  });
+};
 
 const deleteDependente = (request, response) => {
-    const idEmpregado = parseInt(request.params.id) 
-    const nomeDependente = request.params.nomeDependente
+  const idEmpregado = parseInt(request.params.id, 10);
+  const nomeDependente = request.params.nome;
 
-    pool.query('DELETE FROM Dependente WHERE idEmpregado = $1 AND nomeDependente = $2', 
-        [idEmpregado, nomeDependente],
-        (error, result) => {
-            if (error) {
-                response.status(500).send(error);
-                console.log(error);
-            }
+  pool.query('DELETE FROM Dependente WHERE idEmpregado = $1 AND nomeDependente = $2',
+    [idEmpregado, nomeDependente],
+    (error, result) => {
+      if (error) {
+        response.status(500).send(error);
+        console.log(error);
+      }
 
-            response.status(200).send(`Dependente ${nomeDependente} deletado!`)
-        }
-    );
-}
+      console.log(result);
+      response.status(200).send(`Dependente ${nomeDependente} deletado!`);
+    });
+};
 
 const updateDependente = (request, response) => {
-    const idEmpregado = parseInt(request.params.idEmpregado)
-    const nomeDependente = request.params.nomeDependente
+  const idEmpregado = parseInt(request.params.id, 10);
+  const nomeDependente = request.params.nome;
 
-    const { sexo, dtNascimento, parentesco } = request.body
+  const { sexo, dtNascimento, parentesco } = request.body;
 
-    pool.query('UPDATE Dependente SET sexo = $1, parentesco = $2, dtNascimento = $3 ' +
-        'WHERE nome = $4 AND empregado = $5',        
-        [sexo, parentesco, dtNascimento, nomeDependente, idEmpregado],
-        (error, result) => {
-            if (error) {
-                response.status(500).json(error);
-                console.log(error);
-            }
-              
-        response.status(200).send(`Dependente ${nomeDependente} alterado!`)
-        }
-    );
-}
+  pool.query('UPDATE Dependente SET sexo = $1, parentesco = $2, dtNascimento = $3 '
+        + 'WHERE nome = $4 AND empregado = $5',
+  [sexo, parentesco, dtNascimento, nomeDependente, idEmpregado],
+  (error, result) => {
+    if (error) {
+      response.status(500).json(error);
+      console.log(error);
+    }
+
+    console.log(result);
+    response.status(200).send(`Dependente ${nomeDependente} alterado!`);
+  });
+};
 
 const getDepartamentos = (request, response) => {
-    pool.query('SELECT * FROM Departamentos ORDER BY nome',
-        (error, results) => {
-            if (error) {
-                response.status(500).json(error);
-                console.log(error);
-            }
+  pool.query('SELECT * FROM Departamento ORDER BY nome',
+    (error, results) => {
+      if (error) {
+        response.status(500).json(error);
+        console.log(error);
+      }
 
-            response.status(200).json(results.rows);
-        }
-    );
-}
+      response.status(200).json(results.rows);
+    });
+};
+
+const createDepartamento = (request, response) => {
+  const {
+    codigo, nome, gerente, inicioGerente,
+  } = request.body;
+
+  pool.query('INSERT INTO Departamento (codigo, nome, inicio, iniciogerente) '
+        + 'VALUES ($1, $2, $3, $4)', [codigo, nome, gerente, inicioGerente],
+  (error, result) => {
+    if (error) {
+      response.status(500).json(error);
+      console.log(error);
+    }
+
+    console.log(result);
+    response.status(201).send(`Departamento criado com o ID: ${result.insertId}`);
+  });
+};
+
+const deleteDepartamento = (request, response) => {
+  const idDepartamento = parseInt(request.params.id, 10);
+
+  pool.query('DELETE FROM Departamento WHERE codigo = $1', [idDepartamento],
+    (error, result) => {
+      if (error) {
+        response.status(500).json(error);
+        console.log(error);
+      }
+
+      console.log(result);
+      response.status(200).send(`Departamento com ID ${idDepartamento} excluído!`);
+    });
+};
+
+const updateDepartamento = (request, response) => {
+  const idDepartamento = parseInt(request.params.id, 10);
+
+  const { nome, gerente, inicioGerente } = request.body;
+
+  pool.query('UPDATE Departamento SET nome = $1, gerente = $2, iniciogerente = $3 '
+        + 'WHERE codigo = $4', [nome, gerente, inicioGerente, idDepartamento],
+  (error, result) => {
+    if (error) {
+      response.status(500).json(error);
+      console.log(error);
+    }
+
+    console.log(result);
+    response.status(200).send(`Departamento código ${idDepartamento} alterado!`);
+  });
+};
 
 const getLocais = (request, response) => {
-    pool.query('SELECT * FROM Local ORDER BY nome',
-        (error, results) => {
-            if (error) {
-                response.status(500).json(error);
-                console.log(error);
-            }
+  pool.query('SELECT * FROM Local ORDER BY nome',
+    (error, results) => {
+      if (error) {
+        response.status(500).json(error);
+        console.log(error);
+      }
 
-            response.status(200).json(results.rows);
-        }
-    );
-}
+      response.status(200).json(results.rows);
+    });
+};
+
+const createLocal = (request, response) => {
+  const { departamento, nome } = request.body;
+
+  pool.query('INSERT INTO Local (nome, departamento) VALUES ($1, $2)',
+    [nome, departamento],
+    (error, results) => {
+      if (error) {
+        response.status(500).json(error);
+        console.log(error);
+      }
+
+      console.log(results);
+      response.status(201).send(`Local ${nome} criado!`);
+    });
+};
+
+const deleteLocal = (request, response) => {
+  const { nome, departamento } = request.params;
+
+  pool.query('DELETE FROM Local WHERE nome = $1 AND departamento = $2',
+    [nome, departamento],
+    (error, results) => {
+      if (error) {
+        response.status(500).json(error);
+        console.log(error);
+      }
+
+      console.log(results);
+      response.status(200).send(`Local ${nome} deletado!`);
+    });
+};
 
 const getProjetos = (request, response) => {
-    pool.query('SELECT * FROM Projeto ORDER BY nome',
-        (error, results) => {
-            if (error) {
-                response.status(500).json(error).send()
-                console.log(error);
-            }
+  pool.query('SELECT * FROM Projeto ORDER BY nome',
+    (error, results) => {
+      if (error) {
+        response.status(500).json(error).send();
+        console.log(error);
+      }
 
-            response.status(200).json(results.rows)
-        }
-    );
-}
+      response.status(200).json(results.rows);
+    });
+};
 
-module.exports = {
-    createEmpregado,
-    deleteEmpregado,
-    getEmpregados,
-    updateEmpregado,
-    getDependentes,
-    createDependente,
-    deleteDependente,
-    updateDependente,
-    getDepartamentos,
-    getLocais,
-    getProjetos,
-}
+const createProjeto = (request, response) => {
+  const {
+    descricao, codigo, local, departamento,
+  } = request.body;
+
+  pool.query('INSERT INTO Projeto (codigo, local, descricao, departamento) '
+    + 'VALUES ($1, $2, $3, $4)',
+  [codigo, local, descricao, departamento],
+  (error, results) => {
+    if (error) {
+      response.status(500).json(error).send();
+      console.log(error);
+    }
+
+    console.log(results);
+    response.status(201).send(`Projeto com ID ${results.insertId} criado!`);
+  });
+};
+
+const updateProjeto = (request, response) => {
+  const idProjeto = request.params.id;
+  const { local, descricao, departamento } = request.body;
+
+  pool.query('UPDATE Projeto SET local = $1, descricao = $2, departamento = $ '
+    + 'WHERE codigo = $4',
+  [local, descricao, departamento, idProjeto],
+  (error, result) => {
+    if (error) {
+      response.status(500).json(error).send();
+      console.log(error);
+    }
+
+    console.log(result);
+    response.status(200).send(`Projeto com ID ${idProjeto} alterado!`);
+  });
+};
+
+const deleteProjeto = (request, response) => {
+  const idProjeto = request.params.id;
+
+  pool.query('DELETE FROM Projeto WHERE codigo = $1', [idProjeto],
+    (error, result) => {
+      if (error) {
+        response.status(500).json(error).send();
+        console.log(error);
+      }
+
+      console.log(result);
+      response.status(200).send(`Projeto com ID ${idProjeto} deletado!`);
+    });
+};
+
+export default {
+  createEmpregado,
+  deleteEmpregado,
+  getEmpregados,
+  updateEmpregado,
+  getDependentes,
+  createDependente,
+  deleteDependente,
+  updateDependente,
+  getDepartamentos,
+  createDepartamento,
+  deleteDepartamento,
+  updateDepartamento,
+  getLocais,
+  deleteLocal,
+  createLocal,
+  getProjetos,
+  createProjeto,
+  updateProjeto,
+  deleteProjeto,
+};
